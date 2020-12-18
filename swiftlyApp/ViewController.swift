12d457022bb0a85f1539f15f9b466ea7b40bcaae
john edward <dipaaapi8@gmail.com>
2020-12-18 15:02:47 +0800

    Initial Commit

---
+++ swiftlyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swiftlyApp
+//
+//  Created by Dev on 12/18/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
